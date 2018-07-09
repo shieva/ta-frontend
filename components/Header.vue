a4273558912b7d594e7b6bb1f8da3d7dcdb6a65e
Shie <shiannedizo@gmail.com>
2018-07-09 15:05:27 +0800

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="VueToNuxtLogo">
+    <h2>
+    {{title}}
+    </h2>
+  </div>
+</template>
+<script>
+
+export default {
+  data: () => ({
+    title: "Theo"
+  })
+}
+</script>
+
